5c4188220721db80496d26f3e9a55377658b2d61
Kyle Hipolito <kyle.hipolito@quasset.com>
2023-08-23 09:38:05 +0800

    feat: error handling

---
+++ create-modules/hello/hello.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+  "fmt"
+  "log"
+  "example.com/greetings"
+)
+
+func main () {
+  message, err := greetings.Hello("")
+
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  fmt.Println(message)
+}
